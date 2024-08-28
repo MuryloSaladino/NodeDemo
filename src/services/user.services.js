a37ad4480827a0933db2abfe6416c6970bb37034
MuryloSaladino <murylosaladino@gmail.com>
2024-08-28 19:46:22 -0300

    user services

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -9,20 +9,57 @@ export const createUser = async (request, response) => {
     const payload = request.body;
 
     // cryptografando a senha
-    payload.password = hashSync(payload.password)
+    payload.password = hashSync(payload.password);
 
     // criando e salvando o novo usuário
     const user = await User.create(request.body);
 
     // devolvendo o usuário criado para o frontend com o status code mais adequado
-    response.status(201).json(user)
+    // 201 CREATED
+    response.status(201).json(user);
 }
 
-export const findAllUsers = async (req, res) => {
+export const findAllUsers = async (request, response) => {
 
     // encontrando todos os users
     const users = await User.findAll();
 
-    // devolvendo o usuário criado para o frontend com o status code mais adequado
-    res.status(200).json(users)
+    // devolvendo os usuários encontrados para o frontend com o status code mais adequado
+    // 200 OK
+    response.status(200).json(users);
+}
+
+export const findUserById = async (request, response) => {
+
+    // encontrando user pelo id que deve ser passado na url
+    const user = await User.findByPk(request.params.id);
+
+    // devolvendo o usuário encontrado para o frontend com o status code mais adequado
+    // 200 OK
+    response.status(200).json(user);
+}
+
+export const updateUser = async (request, response) => {
+
+    // encontrando o user pelo id que deve ser passado na url
+    const user = await User.findByPk(request.params.id);
+    
+    // método do próprio sequelize para atualizar os campos
+    user.set(request.body);
+    // salvar o objeto após as mudanças
+    await user.save();
+
+    // devolvendo o usuário atualizado para o frontend com o status code mais adequado
+    // 200 OK 
+    response.status(200).json(user);
+}
+
+export const deleteUser = async (request, response) => {
+
+    // encontrando o user pelo id que deve ser passado na url
+    await User.destroy({ where: { id: request.params.id } })
+
+    // devolvendo o usuário atualizado para o frontend com o status code mais adequado
+    // 204 NO CONTENT 
+    response.status(204).send()
 }
