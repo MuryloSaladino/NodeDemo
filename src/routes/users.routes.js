3170dc748907d38ab61057dbc920aa6e03ddd582
MuryloSaladino <murylosaladino@gmail.com>
2024-08-28 19:48:30 -0300

    user routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,16 @@
-import { createUser, findAllUsers } from "../services/user.services";
+import { createUser, deleteUser, findAllUsers, findUserById, updateUser } from "../services/user.services";
 import { Router } from "express";
 
 // instancializando um novo router, um objeto do express que ajuda a separar e configurar as rotas de forma mais flexível
 const userRouter = Router()
 
 // configurando as rotas com o método, o caminho e as funções que o express irá executar como um passo a passo para responder a requisição
-userRouter.post("", createUser)
-userRouter.get("", findAllUsers)
+userRouter.post("", createUser);
+userRouter.get("", findAllUsers);
+// colocando um parâmetro de rota para receber um id na requisição
+userRouter.get("/:id", findUserById);
+userRouter.patch("/:id", updateUser);
+userRouter.delete("/:id", deleteUser);
 
 
 export default userRouter;
