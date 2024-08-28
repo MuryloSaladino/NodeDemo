20d01ae80512803005820fbe2a933193adc491cb
MuryloSaladino <murylosaladino@gmail.com>
2024-08-28 20:04:42 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 // Arquivo específico para instancializar e configurar o express, o coração da API.
 // nada deve ser alterado ou configurado no app fora desse arquivo.
 
-
 import express from "express";
 import userRouter from "./routes/users.routes";
+import cors from "cors"
 
 
 // instanciando o express:
@@ -11,12 +11,14 @@ const app = express();
 // o app.use() configura o app para usar aquela função ou rota, dentro da arquitetura de middlewares.
 // confuso agora, mas pensa que ele faz um passo a passo em cada rota, e você que diz quais passos ele irá "usar" .
 
-app.use("/users", userRouter)
-
-
-
 // aqui estamos configurando o app para ao receber uma requisição, transformar o corpo dele em um json.
 app.use(express.json())
+app.use(cors())
+
+
+// configurando os routers em seus destinos desejados, ou seja, naquele 
+// endereço o router irá atuar adicionando as rotas que ele tem
+app.use("/users", userRouter)
 
 
 export default app;
