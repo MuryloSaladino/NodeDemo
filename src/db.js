4182e4a4d523ab24fa7a1451a49be7ba069d320b
MuryloSaladino <murylosaladino@gmail.com>
2024-08-29 08:43:52 -0300

    dialect from env

--- src/db.js
+++ src/db.js
@@ -5,15 +5,16 @@ const database = process.env.DB_NAME;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const port = process.env.DB_PORT;
+const dialect = process.env.DB_DIALECT
 
 // garantindo que nenhuma é nula ou undefined
-if(!database || !username || !password || !port) throw new Error("Missing .env variable")
+if(!database || !username || !password || !port || !dialect) throw new Error("Missing .env variable")
 
 // criando a conexão com o banco
 const db = new Sequelize( database, username, password, {
     host: "localhost",
     port: Number(port),
-    dialect: "postgres" 
+    dialect: dialect 
 })
 
 export default db;
